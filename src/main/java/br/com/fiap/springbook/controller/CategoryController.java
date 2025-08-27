df7facdf7a7a6e0580f33677d6105438bd80d84f
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-08-27 20:14:45 -0300

    Implementando CRUD ao projeto

--- src/main/java/br/com/fiap/springbook/controller/CategoryController.java
+++ src/main/java/br/com/fiap/springbook/controller/CategoryController.java
@@ -2,23 +2,85 @@ package br.com.fiap.springbook.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.springbook.model.Category;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@RequestMapping("categories")
+@Slf4j
 public class CategoryController {
 
-    @GetMapping("/categories")
-    public List<Category> index() { 
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Cem Anos de Solidão", "Gabriel García Márquez"));
-        categories.add(new Category(2L, "O Senhor dos Anéis", "J.R.R. Tolkien"));
-        categories.add(new Category(3L, "Dom Casmurro", "Machado de Assis"));
+    private List<Category> repository = new ArrayList<>();
 
-        return categories;
+    @GetMapping
+    public List<Category> index(){
+        return repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Category create(@RequestBody Category category){
+        log.info("criando categoria " + category);
+        repository.add(category);
+        return category;
     }
     
+    @GetMapping("{id}")
+    public ResponseEntity<Category> get(@PathVariable Long id){
+        log.info("buscando categoria com id " + id);
+        var categoryFound = getCategoryById(id); 
+
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+
+        return ResponseEntity.ok(categoryFound.get());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> destroy(@PathVariable Long id){
+        log.info("apagando categoria com id {}", id);
+
+        var categoryFound = getCategoryById(id); 
+        
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+
+        repository.remove(categoryFound.get());
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Category> update(@RequestBody Category categoryUpdated, @PathVariable Long id ){
+        log.info("atualizando categoria {} com id {}", categoryUpdated, id);
+
+        var categoryFound = getCategoryById(id); 
+        
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+
+        repository.remove(categoryFound.get());
+        categoryUpdated.setId(id);
+        repository.add(categoryUpdated);
+
+        return ResponseEntity.ok(categoryUpdated);
+    }
+
+    private Optional<Category> getCategoryById(Long id) {
+        var categoryFound = repository.stream()
+            .filter(category -> category.getId().equals(id))
+            .findFirst();
+        return categoryFound;
+    }
 }
--- src/main/java/br/com/fiap/springbook/model/Category.java
+++ src/main/java/br/com/fiap/springbook/model/Category.java
@@ -1,37 +1,12 @@
 package br.com.fiap.springbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Category {
     private Long id;
+    private String gender;
     private String name;
-    private String autor;
-
-    public Category(Long id, String name, String autor) {
-        this.id = id;
-        this.name = name;
-        this.autor = autor;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
 }
